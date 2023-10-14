3078e33f357bf273db257aeeceb185cd4cc7532f
Alessandro Araujo <araujoale688@gmail.com>
2023-10-13 22:53:42 -0300

    SQL, projection, get games by list

--- src/main/java/com/araujoale/dslist/services/GameService.java
+++ src/main/java/com/araujoale/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.araujoale.dslist.dto.GameDTO;
 import com.araujoale.dslist.dto.GameMinDTO;
 import com.araujoale.dslist.entities.Game;
+import com.araujoale.dslist.projections.GameMinProjection;
 import com.araujoale.dslist.repositories.GameRepository;
 
 @Service
@@ -29,6 +30,15 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 
